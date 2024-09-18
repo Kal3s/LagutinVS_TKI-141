1c44cce5dacace7fbe9216d13adfe3adb7ac063f
Вова Лагутин <vs13lag@gmail.com>
2024-09-18 22:18:22 +0300

    Начал делать 2-1

--- Task2-1/Task2-1.c
+++ Task2-1/Task2-1.c
@@ -1,7 +1,58 @@
-я╗┐#include <stdio.h>
+#define _USE_MATH_DEFINES
+#include <stdio.h>
+#include <math.h>
 
+/**
+* @brief Рассчитывает объём шара
+* @param R значение радиуса
+* @return Рассчитанное значение
+*/
+double get_V(double R);
+
+/**
+* @brief Рассчитывает площадь поверхности шара
+* @param R значение радиуса
+* @return Рассчитанное значение
+*/
+double get_S(double R);
+
+/**
+* @brief Точка входа в програsмму
+* @return 0 в случае успеха
+*/
 int main() {
-	printf("Hello,World!");
+	double R;
+	int vybor;
+	printf("Введите радиус: ");
+	scanf_s("%lf", &R);
+	
+	printf("Выберите какую операцию хотите произвести:\n");
+	printf("1. Объем шара\n");
+	printf("2. Площадь поверхности шара\n");
+	printf("(1/2)? ");
+	scanf_s("%d", &vybor);
 
+	if (vybor != 1)
+	{
+		if (vybor != 2){
+			printf("Недопустимое значение. Введите 1 или 2: ");
+			scanf_s("%d", &vybor);
+		}
+	}
+	if (vybor == 2) {
+		printf("Площадь поверхности шара равна: %f \n", get_S(R));
+	}
+	if (vybor == 1) {
+		printf("Объём шара равен: %f \n", get_V(R));
+	}
+	
 	return 0;
+}
+
+double get_V(double R) {
+	return (4  * M_PI * (R * R * R))/ 3;
+}
+
+double get_S(double R) {
+	return (4 * M_PI * R * R);
 }
